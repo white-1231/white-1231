77ccef65dad71f65805702d5f3c6722d617307be
Fey <ff8663521@163.com>
2017-08-29 00:04:38 +0800

    project page

---
+++ routes/project.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.render('project', { title: '项目' });
+  });
+
+module.exports = router;
