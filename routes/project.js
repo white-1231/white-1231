ab537f4f96cc183bf40d70c6858fee9fd5c31d8f
Fey <ff8663521@163.com>
2017-08-29 19:51:46 +0800

    Revert "project page"
    This reverts commit 77ccef65dad71f65805702d5f3c6722d617307be.

--- routes/project.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.render('project', { title: '项目' });
-  });
-
-module.exports = router;
